8fbb3c3fd04d5d923962b1a08c8ca8647be0ea52
TenshiMunasinghe <shadowghost1007@gmail.com>
2023-06-21 21:07:09 +0900

    improve design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { SignInButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Image from "next/image";
+import { FaSignOutAlt } from "react-icons/fa";
 import { api } from "~/utils/api";
 
 const Posts = () => {
@@ -29,33 +30,39 @@ export default function Home() {
       </main>
 
       {!isSignedIn ? (
-        <footer className="fixed bottom-0 left-0 flex flex-col items-center space-y-3 rounded-tr bg-emerald-700 px-9 py-7">
-          <span className="text-xl">Join the Muttering.</span>
+        <footer className="fixed bottom-0 left-0 flex flex-col items-center space-y-3 rounded-tr bg-emerald-700 px-12 py-7">
+          <span className="text-2xl font-bold">Join the Muttering.</span>
           <SignInButton mode="modal">
-            <button className="w-fit rounded bg-gray-100 px-4 py-2 text-emerald-700">
+            <button className="w-fit rounded bg-gray-100 px-5 py-2 text-lg text-emerald-700 ">
               Sign In
             </button>
           </SignInButton>
         </footer>
       ) : (
-        <footer className="fixed bottom-12 left-12 ">
-          <button className="flex items-center space-x-2 rounded-full bg-emerald-200 p-3">
-            <Image
-              src={user.imageUrl}
-              alt="profile pic"
-              width={55}
-              height={55}
-              className="rounded-full"
-            />
-            <div className="text-gray-700">@{user.username}</div>
-          </button>
-          {/* TODO: implement popup for sign out button */}
-          {/* <SignOutButton>
-            <button className="rounded bg-emerald-700 px-6 py-3">
-              Sign Out
-            </button>
-          </SignOutButton> */}
-        </footer>
+        user && (
+          <footer className="fixed bottom-12 left-12">
+            <div className="flex items-center space-x-4 rounded-full p-4 hover:bg-gray-900">
+              <Image
+                src={user.imageUrl}
+                alt="profile pic"
+                width={42}
+                height={42}
+                className="rounded-full"
+              />
+              <div className="flex flex-col">
+                <button className="w-full text-left">Mutter something,</button>
+                <span className="text-left text-gray-400">
+                  @{user.username}
+                </span>
+              </div>
+              <SignOutButton>
+                <button className="h-[42px] w-[42px] rounded-full bg-emerald-700 p-2">
+                  <FaSignOutAlt className="h-full w-full" />
+                </button>
+              </SignOutButton>
+            </div>
+          </footer>
+        )
       )}
     </>
   );
