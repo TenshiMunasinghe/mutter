a54e4954f2e0aff7baed17d5e65345e9a1680bcc
TenshiMunasinghe <shadowghost1007@gmail.com>
2023-08-07 14:34:31 +0900

    implement shadcn form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { useUser } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "~/@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "~/@/components/ui/form";
+import { Input } from "~/@/components/ui/input";
 import Login from "~/components/Login";
 import Post from "~/components/Post";
 import { api } from "~/utils/api";
@@ -18,42 +23,67 @@ const Posts = () => {
   );
 };
 
+const formSchema = z.object({
+  content: z.string().min(1).max(255),
+});
+
 const MakePost = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
   const { user } = useUser();
-  const [input, setInput] = useState("");
   const trpcContext = api.useContext();
   const mutation = api.post.makePost.useMutation({
     async onSuccess() {
-      setInput("");
       await trpcContext.post.getSome.invalidate();
     },
   });
 
   if (!user) return null;
 
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (values) => {
+    mutation.mutate({ content: values.content, userId: user.id });
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        mutation.mutate({ content: input, userId: user.id });
-      }}
-      className="sticky top-0 flex w-full items-stretch space-x-5 border-2 border-t-0 border-gray-600 bg-gray-950/50 p-5 backdrop-blur-sm"
-    >
-      <Image
-        src={user.imageUrl}
-        alt={`profile image of ${user.username || "someone"}`}
-        width={42}
-        height={42}
-        className="h-fit rounded-full"
-      />
-      <input
-        type="text"
-        className="w-full bg-transparent"
-        placeholder="Mutter something."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-    </form>
+    <Form {...form}>
+      <div className="sticky top-0 flex w-full items-stretch space-x-5 border-2 border-t-0 border-gray-600 bg-gray-950/50 p-5 backdrop-blur-sm">
+        <Image
+          src={user.imageUrl}
+          alt={`profile image of ${user.username || "someone"}`}
+          width={42}
+          height={42}
+          className="h-fit rounded-full"
+        />
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex w-full flex-col space-y-4"
+        >
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Mutter something"
+                    autoComplete="off"
+                    className="w-full border-none"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="ml-auto">
+            Mutter
+          </Button>
+        </form>
+      </div>
+    </Form>
   );
 };
 
