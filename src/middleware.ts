23e79c7001cc8ce15cced0759ea93ef545097117
TenshiMunasinghe <shadowghost1007@gmail.com>
2023-08-14 17:38:03 +0900

    make post page public

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ["/", "/api/(.*)"] });
+export default authMiddleware({
+  publicRoutes: ["/", "/api/(.*)", "/post/:id"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
