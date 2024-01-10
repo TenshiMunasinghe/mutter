6cf1d1360b2087ea357e2db96c7c4f54af0c1418
TenshiMunasinghe <shadowghost1007@gmail.com>
2024-01-10 12:14:53 +0900

    wip

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,7 +1,7 @@
 const User = () => {
   return (
     <div className="">
-      <main className="grid grid-cols-4"></main>
+      <main className="grid grid-cols-4">In progress...</main>
     </div>
   );
 };
